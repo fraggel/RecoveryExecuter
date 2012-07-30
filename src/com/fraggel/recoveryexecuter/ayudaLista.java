c395c7807e2fba7c0f32a0ebe27af86c239b2a07
Fraggel <fraggelillo666@hotmail.es>
2012-07-30 17:34:11 +0200

    1.4.0

---
+++ src/com/fraggel/recoveryexecuter/ayudaLista.java
@@ -0,0 +1,20 @@
+package com.fraggel.recoveryexecuter;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.View;
+
+public class ayudaLista extends Activity{
+	AlertDialog diag;
+	 public void onCreate(Bundle savedInstanceState) {
+		 super.onCreate(savedInstanceState);
+		 setContentView(R.layout.ayudalista);
+		 setTitle("Ayuda");
+		 diag=new AlertDialog.Builder(this).create();
+	 }
+	 public void salir(View v){
+		 finish();
+		 
+	 }
+}

79426eb6c02590793438f7125568b10a92de4be2
Fraggel <fraggelillo666@hotmail.es>
2012-08-02 22:23:04 +0200

    1.4.5

---
+++ src/com/fraggel/recoveryexecuter/externalClass.java
@@ -0,0 +1,82 @@
+package com.fraggel.recoveryexecuter;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.SimpleAdapter;
+
+public class externalClass extends Activity{
+
+	public externalClass() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void anyadir(HashMap<String, String> map,ArrayList<String> listaAcciones,AlertDialog diag,Resources res,String file,String selected,String[] items,String[] values,List<HashMap<String, String>> fillMaps,ListView lista,SimpleAdapter adapt,crearLista crearLista){
+		map = new HashMap<String, String>();
+		if(listaAcciones.size()==3){
+			diag.setMessage(res.getString(R.string.msgNoFullLista));
+			diag.setButton(AlertDialog.BUTTON_NEGATIVE,
+					res.getString(R.string.cancelar),crearLista);
+			diag.setButton(AlertDialog.BUTTON_POSITIVE,
+					res.getString(R.string.aceptar),crearLista);
+			diag.show();
+		}
+		else{
+			if (file != null && !"".equals(file)) {
+				listaAcciones.add(file);
+				File ff = new File(file);
+				selected = selected + " " + ff.getName();
+			} else if (!"".equals(selected)) {
+				int r = -1;
+				for (int x = 0; x < items.length; x++) {
+					if (selected.equals(items[x])) {
+						r = x;
+					}
+				}
+				listaAcciones.add(values[r]);
+			}
+			if (!"".equals(selected)) {
+				map.put("rowtexts", selected);
+				fillMaps.add(map);
+			}
+			file = "";
+		}
+		lista.setAdapter(adapt);
+
+	}
+	public void instalarAPK(String ficheroAPK,Resources res,AlertDialog diag,RadioButton rdbNormal,RadioButton rdbSistema,install install) {
+		try {
+			
+			if (ficheroAPK != null && !"".equals(ficheroAPK)
+					&& ficheroAPK.toLowerCase().endsWith(".apk")) {
+				if (rdbNormal.isChecked()) {
+					Intent intent = new Intent(Intent.ACTION_VIEW);
+					intent.setDataAndType(Uri.fromFile(new File(ficheroAPK)),
+							"application/vnd.android.package-archive");
+					startActivity(intent);
+				} else if (rdbSistema.isChecked()) {
+					diag.setMessage(res.getString(R.string.msgNoFull));
+					diag.setButton(AlertDialog.BUTTON_NEGATIVE,
+							res.getString(R.string.cancelar),install);
+					diag.setButton(AlertDialog.BUTTON_POSITIVE,
+							res.getString(R.string.aceptar),install);
+					diag.show();
+				}
+			}
+		} catch (Exception e) {
+			new REException(e);
+
+		}
+
+	}
+
+}
